d0edacf4ab2a6548ccaec0bb035277f3f04b8de3
JueMor <juergen.moroskow@gmail.com>
2023-05-22 16:26:29 +0200

    Refactoring, added Docker Support, Preparing Gateway

---
+++ AccountManagerService/src/test/java/com/example/accountmanager/UserManagerTests.java
@@ -0,0 +1,8 @@
+package com.example.accountmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagerTests {
+}
--- src/main/java/com/example/accountmanager/datatypes/HideId.java
+++
@@ -1,7 +0,0 @@
-package com.example.accountmanager.datatypes;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-@JsonIgnoreProperties({"id", "password"})
-public interface HideId {
-}
